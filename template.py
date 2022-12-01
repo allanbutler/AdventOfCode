c2b8d17bf6452632e508720cfd6b6f1f1b7f0d94
Allan D Butler <allanbutler9@gmail.com>
2022-12-01 15:05:00 -0600

    Create template.py

---
+++ template.py
@@ -0,0 +1,32 @@
+from os import path
+from collections import *
+from networkx import *
+from copy import deepcopy
+import itertools
+import math
+import re
+import pyperclip
+
+
+def ints(string):
+    return list(map(int, re.findall(r"-?[0-9]+", string)))
+
+
+def solve(inp):
+    for line in inp.splitlines():
+        pass
+
+
+example = """\
+"""
+
+if example and not example.isspace():
+    print("Example:", solve(example))
+else:
+    print("No example provided")
+
+with open(path.join(path.dirname(__file__), "input.txt")) as f:
+    result = solve(f.read())
+    print("Output:", result)
+    pyperclip.copy(str(result))
+    print("Copied to clipboard")
